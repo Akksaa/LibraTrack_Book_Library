d4135caba354f304c261914010d0096177ac8af2
Akksaa <shireensaeed786@gmail.com>
2025-03-17 22:34:28 +0500

    added

--- connect.py
+++ connect.py
@@ -4,17 +4,17 @@ import mysql.connector as myconn
 from config import DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USER
 
 
-# my_db = myconn.connect( 
-#     host = DATABASE_HOST,
-#     user = DATABASE_USER,
-#     password = DATABASE_PASSWORD,
-# )
-
-my_db = myconn.connect(
-    host=os.getenv("DATABASE_HOST"),
-    user=os.getenv("DATABASE_USER"),
-    password=os.getenv("DATABASE_PASSWORD"),
+my_db = myconn.connect( 
+    host = DATABASE_HOST,
+    user = DATABASE_USER,
+    password = DATABASE_PASSWORD,
 )
+
+# my_db = myconn.connect(
+#     host=os.getenv("DATABASE_HOST"),
+#     user=os.getenv("DATABASE_USER"),
+#     password=os.getenv("DATABASE_PASSWORD"),
+# )
 
 
 print(my_db, "connection established...")
