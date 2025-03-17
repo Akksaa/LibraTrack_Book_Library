9131b74415309a45d7c4c9c646ec62ae9f425026
Akksaa <shireensaeed786@gmail.com>
2025-03-18 00:24:46 +0500

    updated

--- delete_book.py
+++ delete_book.py
@@ -1,4 +1,5 @@
 import mysql.connector as myconn
+import pandas as pd
 from config import DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_USER
 from select2 import get_all_books
 
@@ -11,14 +12,27 @@ my_db = myconn.connect(
 
 db_cursor = my_db.cursor()
 
+# def delete_book_from_lib(title):
+#     sql = "delete from books where title=%s"
+#     val = (title,)
+
+#     db_cursor.execute(sql,val)
+#     my_db.commit()
+#     result = get_all_books()
+#     print(result)
+
+#     print("Item Deleted!")
+
 def delete_book_from_lib(title):
-    sql = "delete from books where title=%s"
-    val = (title,)
-
-    db_cursor.execute(sql,val)
-    my_db.commit()
-    result = get_all_books()
-    print(result)
-
-    print("Item Deleted!")
-    return result
+    try:
+        sql = "delete from books where title=%s"
+        val = (title,)
+        db_cursor.execute(sql, val)
+        my_db.commit()
+        result = get_all_books()
+        print(result)
+        print("Item Deleted!")
+        return result
+    except Exception as e:
+        print(f"Error in delete_book_from_lib: {e}")
+        return pd.DataFrame()
--- select2.py
+++ select2.py
@@ -12,15 +12,28 @@ my_db = myconn.connect(
 
 db_cursor = my_db.cursor()
 
-def get_all_books():
-    db_cursor.execute("select * from books")
-    results = db_cursor.fetchall()
-    cols = ["id", "title", "author", "genre", "publication_year", "is_read"]
-    # print(pd.DataFrame(results, columns=["id", "title", "author", "genre", "publication_year", "is_read"]))
-    print("from get_all_books():",len(results))
-    if results:
-        return pd.DataFrame(results, columns=cols)
-    return pd.DataFrame()
+# def get_all_books():
+#     db_cursor.execute("select * from books")
+#     results = db_cursor.fetchall()
+#     cols = ["id", "title", "author", "genre", "publication_year", "is_read"]
+#     # print(pd.DataFrame(results, columns=["id", "title", "author", "genre", "publication_year", "is_read"]))
+#     print("from get_all_books():",len(results))
+#     if results:
+#         return pd.DataFrame(results, columns=cols)
+#     return pd.DataFrame()
 
-result = get_all_books()
-print(result)
+# result = get_all_books()
+# print(result)
+
+def get_all_books():
+    try:
+        db_cursor.execute("select * from books")
+        results = db_cursor.fetchall()
+        cols = ["id", "title", "author", "genre", "publication_year", "is_read"]
+        
+        if results:
+            return pd.DataFrame(results, columns=cols)
+        return pd.DataFrame()
+    except Exception as e:
+        print(f"Error in get_all_books: {e}")
+        return pd.DataFrame()
